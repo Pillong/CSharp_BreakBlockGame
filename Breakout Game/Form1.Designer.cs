7a2bebb7f8c331e9549c2e521c246b01ebf51c95
Pillong <jun00su00@naver.com>
2023-05-24 23:46:08 +0900

    초기 커밋.

---
+++ Breakout Game/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Breakout_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(65, 12);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Score : 0";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.Player.Location = new System.Drawing.Point(342, 408);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(100, 30);
+            this.Player.TabIndex = 2;
+            this.Player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tag = "gameTimer";
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Ball.Location = new System.Drawing.Point(378, 204);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 19);
+            this.Ball.TabIndex = 3;
+            this.Ball.TabStop = false;
+            this.Ball.Click += new System.EventHandler(this.Ball_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox Ball;
+    }
+}
+
---
+++ Breakout Game/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout_Game
+{
+
+    public partial class Form1 : Form
+    {
+        int blockCount = 72;
+        bool moveLeft;
+        bool moveRight;
+        bool isGameOver;
+        int score, ballX, ballY, playerSpeed;
+
+        Random random = new Random();
+
+        PictureBox[] blockArray;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            PlaceBlocks();
+        }
+
+
+        private void setupGame()
+        {
+            
+            isGameOver = false;
+            score = 0;
+            ballX = 5;
+            ballY = 5;
+            playerSpeed = 12;
+            txtScore.Text = "Score : " + score;
+
+            Ball.Left = 376;
+            Ball.Top = 250;
+            Player.Left = 347;
+            gameTimer.Start();
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    x.BackColor = Color.FromArgb(random.Next(100, 256), random.Next(100, 256), random.Next(100, 256));
+                }
+            }
+        }
+
+        private void gameOver()
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+            txtScore.Text = "Score : " + score;
+        }
+
+        private void PlaceBlocks()
+        {
+            
+            blockArray = new PictureBox[blockCount];
+            int a = 0;
+            int top = 10;
+            int left = 100;
+
+            for (int i = 0; i < blockArray.Length; i++)
+            {
+                blockArray[i] = new PictureBox();
+                blockArray[i].Height = 25;
+                blockArray[i].Width = 81;
+                blockArray[i].Tag = "blocks";
+                blockArray[i].BackColor = Color.White;
+
+                if (a == 9)
+                {
+                    top = top + 30;
+                    left = 100;
+                    a = 0;
+                }
+
+                if (a < 9)
+                {
+                    a++;
+                    blockArray[i].Left = left;
+                    blockArray[i].Top = top;
+                    this.Controls.Add(blockArray[i]);
+                    left = left + 100;
+                }
+            }
+            setupGame();
+        }
+
+        private void removeBlocks()
+        {
+            foreach(PictureBox x in blockArray)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+
+        private void Ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score : " + score;
+
+            if(moveLeft == true &&  Player.Left > 0)
+            {
+                Player.Left -= playerSpeed;
+            }
+            if(moveRight == true &&  Player.Left < 700)
+            {
+                Player.Left += playerSpeed;
+            }
+
+            Ball.Left += ballX;
+            Ball.Top += ballY;
+
+            if(Ball.Left < 0 || Ball.Left > 775)
+            {
+                ballX = -ballX;
+            }
+            if(Ball.Top < 0)
+            {
+                ballY = -ballY;
+            }
+
+            if(Ball.Bounds.IntersectsWith(Player.Bounds))
+            {
+
+                Ball.Top = 390;
+
+                ballY = random.Next(8, 12) * -1;
+                
+                if(ballX < 0)
+                {
+                    ballX = random.Next(8, 12) * -1;
+                }
+                else
+                {
+                    ballX = random.Next(-8, 12);
+                }
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "blocks")
+                {
+                    if(Ball.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        score += 1;
+                        ballY = -ballY;
+                        this.Controls.Remove(x);
+
+                    }
+                }
+            }
+
+            if(score == blockCount)
+            {
+                gameOver();
+                MessageBox.Show("Your Score : " + score + ".\nPress 'Enter' to play Again", "You Win!!");
+
+            }
+            if(Ball.Top > 470)
+            {
+                gameOver();
+                MessageBox.Show("Your Score : " + score + ".\nPress 'Enter' to try Again", "You Lose..");
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeBlocks();
+                PlaceBlocks();
+            }
+        }
+    }
+}
